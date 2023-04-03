59c5ba2d555d7a0c2499af659ba679b470c7b447
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-04-04 00:37:21 +0200

    Add constant files.

---
+++ src/main/java/chemical/pursuit/constant/CollectionNames.java
@@ -0,0 +1,9 @@
+package chemical.pursuit.constant;
+
+public final class CollectionNames {
+
+    private CollectionNames() {
+    }
+
+    public static final String QUESTION = "questions";
+}
---
+++ src/main/java/chemical/pursuit/constant/Paths.java
@@ -0,0 +1,10 @@
+package chemical.pursuit.constant;
+
+public final class Paths {
+
+    private Paths() {
+    }
+
+    public static final String QUESTION = "/question";
+    public static final String QUESTION_CATEGORY = "/question/category";
+}
